6af31c69da1139334f2a16c7d08cfd6a67e0b91f
Ismael Barros <razielmine@gmail.com>
2013-03-21 15:00:59 +0100

    snipplet to run shell commands in xterm if the package is not being run in a shell

--- util.sh
+++ util.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 run_oss() { if [ $(which padspp 2>/dev/null) ]; then padsp $@; else $@; fi; }
+run_shell() { if [[ $(tty) = "not a tty" ]]; then xterm -e "$@"; else $@; fi; }
 
 get_resolution() { xrandr | grep \* | cut -d' ' -f4; }
 set_resolution() { xrandr -s $1; }

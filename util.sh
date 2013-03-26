626cc3a4eb5d045f13a1199043048ff8b5df700f
Ismael Barros <razielmine@gmail.com>
2013-03-26 16:14:00 +0100

    Copule tweaks

--- util.sh
+++ util.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-run_oss() { if [ $(which padspp 2>/dev/null) ]; then padsp $@; else $@; fi; }
-run_shell() { if [[ $(tty) = "not a tty" ]]; then xterm -e "$@"; else $@; fi; }
-
 get_resolution() { xrandr | grep \* | cut -d' ' -f4; }
 set_resolution() { xrandr -s $1; }
+
+run_oss() { if [ $(which padspp 2>/dev/null) ]; then padsp $@; else $@; fi; }
+run_shell() { if [[ $(tty) = "not a tty" ]]; then xterm -e "$@"; else $@; fi; }
+run_keepResolution() {
+	resolution=$(get_resolution)
+	trap atexit set_resolution "$resolution"
+	$@
+	set_resolution "$resolution"	
+}
